206e2b3ab8cc17de4262497298a36d0f66eb24d7
samudithachandrakantha <samudithasamuditha44@gmail.com>
2026-05-17 14:35:27 +0530

    created folder structure created login and dashboard ui

--- app/src/main/java/com/hfad/agencyapp/MainActivity.java
+++ app/src/main/java/com/hfad/agencyapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.hfad.agencyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.hfad.agencyapp.ui.auth.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Navigate to LoginActivity
+        // Note: User must configure Firebase (google-services.json) first
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
-//chdfgkdjvfkdvhnlk
+
+
